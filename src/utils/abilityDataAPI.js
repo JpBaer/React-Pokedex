5d2ca0504b232a93bf534c57772e7fae0dbee4a0
Jorgen Baertsch <jorgenpaulmusic@gmail.com>
2023-05-23 09:50:06 -0700

    Updates

--- src/utils/abilityDataAPI.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-const fetchAbilityData = (ability) =>
-    axios.get(`https://pokeapi.co/api/v2/ability/${ability}`)
-
-export default fetchAbilityData
--- src/utils/singlePokemonAPI.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const singlePokemonFetch = (id) => 
-axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-
-export default singlePokemonFetch
-
